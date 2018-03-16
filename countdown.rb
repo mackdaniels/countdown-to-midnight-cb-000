7146e10faee5f9d883348ced3e4e4261acce295e
Nicholas Daniel <mackdaniels@protonmail.com>
2018-03-16 03:33:14 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  while n >= 1
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+countdown(10)
+
+def countdown_with_sleep(n)
+  while n >= 1
+    puts "#{n} SECOND(S)!"
+    n -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
 end
